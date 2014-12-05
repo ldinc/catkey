61f58fb90ee5479b791b2fa1caaf04f457f53af0
ldinc <drogunov.igor@gmail.com>
2014-12-05 23:59:44 +0300

    example init body

---
+++ example/example.c
@@ -0,0 +1,8 @@
+#include "example.h"
+
+int main()
+{
+	printf("Hello, this is catkey example!!!!\n");
+
+	return 0;
+}
---
+++ example/example.h
@@ -0,0 +1,7 @@
+#ifndef __CATKEY_H__
+#define __CATKEY_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif
